6ddbbf98e3ca3e35a552f7d6a572655e60721274
arye321 <arye321@users.noreply.github.com>
2021-04-01 14:41:57 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
-fetch('https://www.reddit.com/.json')
-  .then(response => response.text())
-  .then((data) => {
-    console.log(data)
-  })
+// learn jquery ajax 
+// http://net.tutsplus.com/tutorials/javascript-ajax/5-ways-to-make-ajax-calls-with-jquery/
+
+// no need to specify document ready
+
+$(function(){
+    // don't cache ajax or content won't be fresh
+    $.ajaxSetup ({
+        cache: false
+    });
+    var ajax_load = "<img src='http://i.imgur.com/pKopwXp.gif' alt='loading...' />";
+    
+    // load() functions
+    var loadUrl = "http://fiddle.jshell.net/dvb0wpLs/show/";
+    $("#loadbasic").click(function(){
+        $("#result").html(ajax_load).load(loadUrl);
+    });
+
+// end  
+});
