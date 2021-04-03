fd2be11bf26ecca7b9c6ed8bf2c57d5d4b5e6d1d
arye321 <arye321@users.noreply.github.com>
2021-04-03 19:43:32 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,20 +1,3 @@
-// learn jquery ajax 
-// http://net.tutsplus.com/tutorials/javascript-ajax/5-ways-to-make-ajax-calls-with-jquery/
-
-// no need to specify document ready
-
-$(function(){
-    // don't cache ajax or content won't be fresh
-    $.ajaxSetup ({
-        cache: false
-    });
-    var ajax_load = "<img src='http://i.imgur.com/pKopwXp.gif' alt='loading...' />";
-    
-    // load() functions
-    var loadUrl = "http://fiddle.jshell.net/dvb0wpLs/show/";
-    $("#loadbasic").click(function(){
-        $("#result").html(ajax_load).load(loadUrl);
-    });
-
-// end  
-});
+fetch('https://opadendi.epizy.com/lol.txt')
+  .then(response => response.json())
+  .then(data => console.log(data));
